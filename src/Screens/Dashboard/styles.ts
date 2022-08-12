dab75a54f863173dd967e16648f93d4b5dc2e90e
gabrieli <gabrieli.martins@visie.com>
2022-08-12 15:16:37 -0300

    Criando estilos globais

--- src/Screens/Dashboard/styles.ts
+++ src/Screens/Dashboard/styles.ts
@@ -4,6 +4,8 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const Title = styled.Text``;
