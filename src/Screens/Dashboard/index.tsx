5e8c1afbd7271881564a74d5e552f65c9a47d16c
gabrieli <gabrieli.martins@visie.com>
2022-08-12 21:40:05 -0300

    utilizando icones

--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Icon,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>GoFinances</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/62737286?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Gabi</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
