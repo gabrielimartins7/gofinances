e571ab30259cc02dbc23027abb45dfac92037f5d
gabrieli <gabrieli.martins@visie.com>
2022-08-16 19:39:29 -0300

    Posicionando os cartões em destaque

---
+++ README.md
@@ -0,0 +1,41 @@
+# Mobile Developer
+
+> In this readme I will help you with the installation and execution of the project step by step.
+
+## 💻 Requirements (libs, dependencies, packages)
+
+- [x] Npm or Yarn
+- [x] Expo
+- [x] React Native
+- [x] Typescript
+- [x] React native responsive fontsize
+- [x] React native iphone X helper
+- [x] Styled components
+- [x] Expo vector icons
+
+Antes de começar, verifique se você atendeu aos seguintes requisitos:
+
+- You've installed in your machine the latest version of:
+  ` react-native / expo / npm or yarn / VSCode or another IDE`
+
+## 🚀 Installing
+
+For intalling , follow these steps:
+
+Git Clone:
+
+```
+git clone https://github.com/gabrielimartins7/gofinances
+```
+
+Enter the repository:
+
+```
+cd gofinances
+```
+
+Open the project:
+
+```
+code .
+```
--- src/Screens/Dashboard/styles.ts
+++ src/Screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
   flex: 1;
@@ -18,7 +19,7 @@ export const Header = styled.View`
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
-  margin-top: 70px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
 
   flex-direction: row;
   justify-content: space-between;
@@ -62,4 +63,8 @@ export const HighlightCards = styled.ScrollView.attrs({
   horizontal: true,
   showsHorizontalScrollIndicator: false,
   contentContainerStyle: { paddingHorizontal: 24 },
-})``;
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
