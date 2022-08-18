0a5d31c9444fe3e6b1205af8210e222e138767d4
gabrieli <gabrieli.martins@visie.com>
2022-08-18 20:00:59 -0300

    Finalizando a listagem de transações

--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { getBottomSpace } from "react-native-iphone-x-helper";
 
 import { HighlightCard } from "../../Components/HighlightCard";
-import { TransactionCard } from "../../Components/TransactionCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../Components/TransactionCard";
 
 import {
   Container,
@@ -20,9 +22,14 @@ import {
   Title,
 } from "./styles";
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
-  const data = [
+  const data: DataListProps[] = [
     {
+      id: "1",
       type: "positive",
       title: "Desenvolvimento de App",
       amount: "R$ 12.000,00",
@@ -33,6 +40,7 @@ export function Dashboard() {
       date: "17/08/2022",
     },
     {
+      id: "2",
       type: "negative",
       title: "Ramis Hamburgueria",
       amount: "R$ 59,00",
@@ -43,6 +51,7 @@ export function Dashboard() {
       date: "10/08/2022",
     },
     {
+      id: "3",
       type: "negative",
       title: "Aluguel do apartamento",
       amount: "R$ 1.200,00",
@@ -98,11 +107,8 @@ export function Dashboard() {
         <Title>Listagem</Title>
         <TransactionList
           data={data}
+          keyExtractor={(item) => item.id}
           renderItem={({ item }) => <TransactionCard data={item} />}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingBottom: getBottomSpace(),
-          }}
         />
       </Transaction>
     </Container>
--- src/Screens/Dashboard/styles.ts
+++ src/Screens/Dashboard/styles.ts
@@ -1,7 +1,13 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
+
+import { DataListProps } from ".";
 
 export const Container = styled.View`
   flex: 1;
@@ -82,4 +88,11 @@ export const Title = styled.Text`
   margin-bottom: 16px;
 `;
 
-export const TransactionList = styled.FlatList``;
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})``;
