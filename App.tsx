5090005aa2c95e11e52d42a3dd96e14614e9b244
gabrieli <gabrieli.martins@visie.com>
2022-08-19 14:48:44 -0300

    Componente de Input

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
 
 import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/Screens/Dashboard";
+import { Register } from "./src/Screens/Register";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
