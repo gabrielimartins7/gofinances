7fca04b563330c890de6bf23e9ab8747d973a10d
gabrieli <gabrieli.martins@visie.com>
2022-09-19 17:07:29 -0300

    finalizando RectButton e BorderlessButton

--- src/Components/Forms/TransactionTypeButton/index.tsx
+++ src/Components/Forms/TransactionTypeButton/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { TouchableOpacityProps } from "react-native";
+import { RectButtonProps } from "react-native-gesture-handler";
 
-import { Container, Icon, Title } from "./styles";
+import { Container, Icon, Title, Button } from "./styles";
 
 const icons = {
   up: "arrow-up-circle",
   down: "arrow-down-circle",
 };
 
-interface Props extends TouchableOpacityProps {
+interface Props extends RectButtonProps {
   type: "up" | "down";
   title: string;
   isActive: boolean;
@@ -21,9 +21,11 @@ export function TransactionTypeButton({
   ...rest
 }: Props) {
   return (
-    <Container isActive={isActive} type={type} {...rest}>
-      <Icon name={icons[type]} type={type} />
-      <Title>{title}</Title>
+    <Container isActive={isActive} type={type}>
+      <Button {...rest}>
+        <Icon name={icons[type]} type={type} />
+        <Title>{title}</Title>
+      </Button>
     </Container>
   );
 }
--- src/Components/Forms/TransactionTypeButton/styles.ts
+++ src/Components/Forms/TransactionTypeButton/styles.ts
@@ -12,18 +12,12 @@ interface ContainerProps {
   type: "up" | "down";
 }
 
-export const Container = styled(RectButton)<ContainerProps>`
+export const Container = styled.View<ContainerProps>`
   width: 48%;
-
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
 
   border-width: ${({ isActive }) => (isActive ? 0 : 1.5)}px;
   border-style: solid;
   border-color: ${({ theme }) => theme.colors.text};
-
-  padding: 16px;
 
   ${({ isActive, type }) =>
     isActive &&
@@ -38,6 +32,14 @@ export const Container = styled(RectButton)<ContainerProps>`
     css`
       background-color: ${({ theme }) => theme.colors.attention_light};
     `};
+`;
+
+export const Button = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  padding: 16px;
 `;
 
 export const Icon = styled(Feather)<IconProps>`
