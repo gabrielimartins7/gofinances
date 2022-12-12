7844e09b8a38933dcd21b2892bca211e85a45e6f
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2022-12-12 20:24:39 -0300

    Interface de autenticação

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -20,19 +20,20 @@ export function SignIn(){
             <Header>
                 <TitleWrapper>
                     <LogoSvg 
-                        width={RFValue(200)}
-                        height={RFValue(200)}
+                        width={RFValue(120)}
+                        height={RFValue(68)}
                     />
                     <Title>
-                        Controle suas
-                        finanças de forma
-                        muito simples
+                        Controle suas {'\n'}
+                        finanças de forma {'\n'}
+                        muito simples 
                     </Title>
                 </TitleWrapper>
 
                 
                 <SignInTitle>
-                    Faça seu login com uma das contas abaixo
+                    Faça seu login com {'\n'} 
+                    uma das contas abaixo
                 </SignInTitle>
             </Header>
             <Footer></Footer>
--- src/Screens/SignIn/styles.ts
+++ src/Screens/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
     flex: 1;
@@ -7,12 +8,40 @@ export const Container = styled.View`
 export const Header = styled.View`
     width: 100%;
     height: 70%;
+
+    background-color: ${({ theme }) => theme.colors.primary};
+    justify-content: flex-end;
+    align-items: center;
 `;
 
-export const TitleWrapper = styled.View``;
+export const TitleWrapper = styled.View`
+    align-items: center;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+    font-size: ${RFValue(30)}px;
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: ${({ theme }) => theme.colors.shape};
 
-export const SignInTitle = styled.Text``;
+    text-align: center;
 
-export const Footer = styled.View``;
+    margin-top: 45px;
+`;
+
+export const SignInTitle = styled.Text`
+    font-size: ${RFValue(16)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+    color: ${({ theme }) => theme.colors.shape};
+
+    text-align: center;
+
+    margin-top: 80px;
+    margin-bottom: 67px;
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+    height: 30%;
+
+    background-color: ${({ theme }) => theme.colors.secondary};
+`;
