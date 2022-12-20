b07ecaa95d16de735c323a5fbb8b8077278e6aac
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2022-12-20 12:21:34 -0300

    conectando com uma conta google

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React from "react";
+import { Alert } from 'react-native';
 import { RFValue } from "react-native-responsive-fontsize";
 
 import AppleSvg from "../../assets/apple.svg";
@@ -20,8 +21,16 @@ import {
  } from "./styles";
 
 export function SignIn(){
-    const { user } = useAuth();
-    console.log(user);
+    const { signInWithGoogle } = useAuth();
+
+    async function handleSignInWithGoogle() {
+        try {
+            await signInWithGoogle();
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Não foi possível conectar a conta Google');
+        }
+    }
 
     return(
         <Container>
@@ -49,6 +58,7 @@ export function SignIn(){
                     <SignInSocialButton 
                         title="Entrar com o Google"
                         svg={GoogleSvg}
+                        onPress={handleSignInWithGoogle}
                     />
                     <SignInSocialButton 
                         title="Entrar com o Apple"
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, ReactNode, useContext } from "react";
 
+import * as AuthSession from 'expo-auth-session';
+
 interface AuthProviderProps{
     children: ReactNode;
 }
@@ -13,6 +15,7 @@ interface User {
 
 interface AuthContextData {
     user: User;
+    signInWithGoogle(): Promise<void>;
 }
 
 const AuthContext = createContext({} as AuthContextData);
@@ -24,8 +27,28 @@ function AuthProvider({ children }: AuthProviderProps ){
         email: 'gabi@email.com',
     };
 
+    async function signInWithGoogle() {
+        try {
+            const CLIENT_ID = '612246367141-mjfe1clmhppgn1ug98op6fqph8si73hr.apps.googleusercontent.com';
+            const REDIRECT_URI = 'https://auth.expo.io/@gabrielimartins07/gofinances';
+            const RESPONSE_TYPE ='token';
+            const SCOPE = encodeURI('profile email');
+
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}$scope=${SCOPE}`;
+            
+            const response = await AuthSession.startAsync({ authUrl });
+            console.log(response);
+
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
     return(
-        <AuthContext.Provider value={{ user }}>
+        <AuthContext.Provider value={{ 
+            user, 
+            signInWithGoogle 
+        }}>
             { children }
         </AuthContext.Provider>
     )
