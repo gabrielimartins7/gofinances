f2bb89e91d6b6e1a906bbd32e2f03134de9c8b09
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2022-12-22 15:16:18 -0300

    Conectando com uma conta google, e refactor

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext } from "react";
+import React, { createContext, ReactNode, useContext, useState } from "react";
 
 import * as AuthSession from 'expo-auth-session';
 
@@ -18,14 +18,17 @@ interface AuthContextData {
     signInWithGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+    params: {
+        access_token: string;
+    },
+    type: string;
+}
+
 const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps ){
-    const user = {
-        id: '1106',
-        name: 'Gabi Martins',
-        email: 'gabi@email.com',
-    };
+    const [user, setUser] = useState<User>({} as User);
 
     async function signInWithGoogle() {
         try {
@@ -34,10 +37,22 @@ function AuthProvider({ children }: AuthProviderProps ){
             const RESPONSE_TYPE ='token';
             const SCOPE = encodeURI('profile email');
 
-            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}$scope=${SCOPE}`;
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
             
-            const response = await AuthSession.startAsync({ authUrl });
-            console.log(response);
+            const { type, params } = await AuthSession
+            .startAsync({ authUrl }) as AuthorizationResponse;
+
+            if( type === 'success') {
+                const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+                const userInfo = await response.json();
+                
+                setUser({
+                    id: userInfo.id,
+                    email: userInfo.email,
+                    name: userInfo.given_name,
+                    photo: userInfo.picture
+                });
+            }
 
         } catch (error) {
             throw new Error(error);
