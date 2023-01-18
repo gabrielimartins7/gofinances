a16ca19ea82551d94d60bd4228097ed649b306df
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2023-01-18 16:53:09 -0300

    Opções de autenticação na plataforma

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ActivityIndicator, Alert } from 'react-native';
+import { ActivityIndicator, Alert, Platform } from 'react-native';
 import { RFValue } from "react-native-responsive-fontsize";
 import { useTheme } from 'styled-components';
 
@@ -78,11 +78,14 @@ export function SignIn(){
                         svg={GoogleSvg}
                         onPress={handleSignInWithGoogle}
                     />
-                    <SignInSocialButton 
-                        title="Entrar com o Apple"
-                        svg={AppleSvg}
-                        onPress={handleSignInWithApple}
-                    />
+                    {
+                        Platform.OS === 'ios' &&
+                        <SignInSocialButton 
+                            title="Entrar com o Apple"
+                            svg={AppleSvg}
+                            onPress={handleSignInWithApple}
+                        />
+                    }
                 </FooterWrapper>
                 { isLoading && 
                     <ActivityIndicator 
