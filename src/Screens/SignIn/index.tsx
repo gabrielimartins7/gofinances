4712166bc0e905553cf93ed129f911ee10d64d9a
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2023-01-18 16:48:26 -0300

    Corrigindo load do login

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -35,7 +35,6 @@ export function SignIn(){
         } catch (error) {
             console.log(error);
             Alert.alert('Não foi possível conectar a conta Google');
-        } finally {
             setIsLoading(false);
         }
     }
@@ -47,7 +46,6 @@ export function SignIn(){
         } catch (error) {
             console.log(error);
             Alert.alert('Não foi possível conectar a conta Apple');
-        } finally {
             setIsLoading(false);
         }
     }
